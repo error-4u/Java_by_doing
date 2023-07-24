ff9c9e8caf8b93849d44c2126f9eaf1a8ca4fc1f
vboxuser <vboxuser@ubuntu1.myguest.virtualbox.org>
2023-07-24 21:58:54 +0530

    loops in java

---
+++ src/Loops.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+
+//        for loops
+        // print number in for loops
+
+//
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//        for (int num = 1; num <= n; num++){
+//            System.out.println(num);
+
+        // using while loop
+
+//        int a = 1;
+//        while ( a <= 5){
+//            System.out.println(a);
+//            a ++;
+
+
+        // using do while loop
+
+        int n = 1;
+
+
+    }
+}
---
+++ src/Primitives.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+/*  Primitives are the data type that cannot be further divided like for example
+*
+* int a = 24;
+* char name = 'c';
+* float marks = 94.43;
+*
+* these are the data types that cannot be further divided
+*
+* But unlike string it is a non primitive data type
+*
+* char name = "India"
+*
+* it can be further divided into char so it is not a primitive data type
+*
+*
+*
+*
+*
+* */
+
